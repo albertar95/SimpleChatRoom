dec967c1f0d7685f1eaa944a0707b5cfda90ce64
albertar95 <alb.rajabi@gmail.com>
2023-01-09 18:52:53 +0330

    project done

---
+++ Content/Scripts/CheckName.js
@@ -0,0 +1,9 @@
+﻿function Check(input)
+{
+    var names = ['ali', 'lav', 'mosy', 'mehdi', 'hossein']
+    var index = names.indexOf(input);
+    if (index == -1)
+        return false;
+    else
+        return true;
+}
